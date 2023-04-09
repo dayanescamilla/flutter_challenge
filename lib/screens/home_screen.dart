c0f89bab4840a1bb0a430e6e1c14cc1ab8c9ba02
Dayan Escamilla <josedayanescamillaperez@gmail.com>
2023-04-08 19:09:33 -0600

    - Added initState - The main Widget was set to StatefulWidget to be able to update changes on the screen. - A Future was added to be able to work with the help of the Dio package and response to be able to establish a response. - Added an if condition - setState is set - Added an animation
    Signed-off-by: Dayan Escamilla <josedayanescamillaperez@gmail.com>

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
-import 'dart:math';
+import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,6 +12,33 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  String? information;
+  final bool _bool = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getRequest();
+  }
+
+  Future getRequest() async {
+    Response response;
+    response = await Dio().get('http://numbersapi.com/random');
+
+    var data = await json.decode(json.encode(response.data));
+
+    information = data;
+    return information;
+
+    // var url = Uri.parse('http://numbersapi.com/random');
+    // var response = await http.get(url);
+    //
+    // var data = await json.decode(json.encode(response.body));
+    // information = data;
+    // return information;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,22 +49,43 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            if (information != null)
             Container(
               alignment: Alignment.center,
               width: double.infinity,
-              height: 400,
-              child: const Card(
+              child: Card(
                 elevation: 10,
-                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Center(child: Text('Aqui va un texto', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w100),)),
+                margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Container(
+                  padding: const EdgeInsetsDirectional.symmetric(horizontal: 20, vertical: 5),
+                  child: AnimatedCrossFade(
+                    crossFadeState: _bool
+                        ? CrossFadeState.showSecond
+                        : CrossFadeState.showFirst,
+                    duration: const Duration(milliseconds: 350),
+                    firstChild: Text(
+                      information!,
+                      overflow: TextOverflow.clip,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w200),
+                    ),
+                    secondChild: Text(
+                      information!,
+                      overflow: TextOverflow.clip,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w200),
+                    ),
+                  ),
+                ),
               ),
             )
           ],
-        )
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('Hola:)');
+        onPressed: () async {
+          setState(() {});
+          information = await getRequest();
         },
         child: const Icon(Icons.change_circle, size: 30),),
     );
